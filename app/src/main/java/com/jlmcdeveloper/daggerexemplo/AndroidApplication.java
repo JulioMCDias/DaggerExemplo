7dcb6261d86eb2328c29e420f23243426f2f8205
JulioMCDias <juliomarques11@gmail.com>
2020-01-14 16:36:58 -0300

    create project

---
+++ app/src/main/java/com/jlmcdeveloper/daggerexemplo/AndroidApplication.java
@@ -0,0 +1,11 @@
+package com.jlmcdeveloper.daggerexemplo;
+
+import android.app.Application;
+
+public class AndroidApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
---
+++ app/src/main/java/com/jlmcdeveloper/daggerexemplo/ui/MainActivity.java
@@ -0,0 +1,25 @@
+package com.jlmcdeveloper.daggerexemplo.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.jlmcdeveloper.daggerexemplo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void btnAddCar(View view){
+
+    }
+
+    public void btnListCar(View view){
+
+    }
+}
