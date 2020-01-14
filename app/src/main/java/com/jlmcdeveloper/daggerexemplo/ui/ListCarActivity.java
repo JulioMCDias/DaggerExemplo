76ef8a871cc8f9646f787237d9e53c522832f0f4
JulioMCDias <juliomarques11@gmail.com>
2020-01-14 19:54:20 -0300

    add view add car

--- app/src/main/java/com/jlmcdeveloper/daggerexemplo/ui/ListCarActivity.java
+++ app/src/main/java/com/jlmcdeveloper/daggerexemplo/ui/ListCarActivity.java
@@ -1,31 +1,26 @@
 package com.jlmcdeveloper.daggerexemplo.ui;
 
 import android.os.Bundle;
-import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.view.View;
+
 import com.jlmcdeveloper.daggerexemplo.R;
 
-public class AddCarActivity extends AppCompatActivity {
+public class ListCarActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_car);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_list_car);
+        Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
-    }
-
-
-    public void btnSaveCar(View view) {
 
     }
 
-
-    public void btnCancel(View view) {
-
-    }
 }
